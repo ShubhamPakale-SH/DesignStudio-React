e97f5b07812eb68e5f6d0cf1ed3616813d6460af
ShubhamPakale-SH <shubham.pakale@simplifyhealthcare.com>
2026-05-27 07:14:57 +0000

    Delete Temp directory

--- Temp/Test/SimpleApi/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/hello", () => new { message = "Hello, World!" });
-
-app.Run();
--- Temp/kafka/SecureBank/account-service/src/main/java/com/securebank/account/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.securebank.account.repository;
-
-import com.securebank.account.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-}
