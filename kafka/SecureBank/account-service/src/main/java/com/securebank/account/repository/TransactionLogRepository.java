d6bc7518e8ff2ed454675dff76d29c2442615831
ShubhamPakale-SH <shubham.pakale@simplifyhealthcare.com>
2026-05-31 20:14:59 +0000

    Delete kafka directory

--- kafka/SecureBank/account-service/src/main/java/com/securebank/account/repository/TransactionLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.securebank.account.repository;
-
-import com.securebank.account.model.TransactionLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionLogRepository extends JpaRepository<TransactionLog, String> {
-}
